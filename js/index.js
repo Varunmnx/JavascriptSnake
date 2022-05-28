718a08986a7214cf1f55e9d7a758eb6e3aa4219c
Varunmnx <varun.cheriyath@gmail.com>
2022-05-28 16:31:43 +0530

    js project

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let directions ={'x':0,'y':0};
+let foodSound =new Audio('../music/food.mp3');
+let gameOverSound = new Audio('../music/gameover.mp3');
+let moveSound  = new Audio('../music/move.mp3')
+let musicSound = new Audio("../music/music.mp3")
